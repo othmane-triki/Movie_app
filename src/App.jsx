bb2fad659348e3cb2b18cffff2b24bfc5763f958
othmane <trikiotmane@gmail.com>
2025-12-14 01:32:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className="test text-3xl font-bold flex justify-center items-center min-h-screen bg-stone-500">red text</div>
+    </>
+  )
+}
+
+export default App
